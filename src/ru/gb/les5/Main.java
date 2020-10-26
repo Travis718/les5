f06ca43372c5b77f81d4add29271903cfffa7b53
Travis <suporal@mail.ru>
2020-10-27 00:48:21 +0300

    В консоль не выводится. хз

--- src/ru/gb/les5/Main.java
+++ src/ru/gb/les5/Main.java
@@ -1,9 +1,57 @@
 package ru.gb.les5;
 
-public class Main
-{
-    public static void main(String[] args)
-    {
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+
+
+
+        Worker[] worker = new Worker[5];
+        worker[0] =
+                new Worker(
+                        "Иванов Иван Иваныч",
+                        "Директор",
+                        "Ivanjv@mail.ru",
+                        "+7-910-123-28-45",
+                        100000,
+                        28);
+
+        worker[1] =
+                new Worker(
+                        "Зюзельман Пафнутий Васгенович",
+                        "Юрисконсульт",
+                        "Monster@bk.ru",
+                        "+7-910-128-22-37",
+                        50000,
+                        35);
+
+        worker[2] =
+                new Worker(
+                        "Утыркина Прасковья Феофановна",
+                        "Главный бухгалтер",
+                        "Boginya69@yandex.ru",
+                        "+7-919-177-71-77",
+                        35000,
+                        51);
+        worker[3] =
+                new Worker(
+                        "Гатыло Остап Богданович",
+                        "Учредитель",
+                        "123456789@mail.ru",
+                        "+7-917-777-77-77",
+                        1000000000,
+                        47);
+        worker[4] =
+                new Worker(
+                        "Сидоров Сидор Сидорович",
+                        "Человек-оркестр",
+                        "CosaSidorova@gmail.com",
+                        "+7-905-862-14-36",
+                        15000,
+                        35);
+
 
     }
+
 }
--- src/ru/gb/les5/Worker.java
+++ src/ru/gb/les5/Worker.java
@@ -1,11 +1,40 @@
 package ru.gb.les5;
 
+import java.util.Scanner;
+
 public class Worker
 {
-    String name;
-    String position;
-    String email;
-    String phone;
-    String salary;
-    String age;
+     String name;
+     String position;
+     String email;
+     String phone;
+     int salary;
+     int age;
+
+    public Worker(String name, String position, String email, String phone, int salary, int age)
+    {
+        this.name = name;
+        this.position = position;
+        this.email = email;
+        this.phone = phone;
+        this.salary = salary;
+        this.age = age;
+    }
+
+
+    public void printInfo()
+    {
+        System.out.print("\n Имя : " + name);
+        System.out.print("\n Должность : " + position);
+        System.out.print("\n Электронная почта : " + email);
+        System.out.print("\n Номер телефона : " + phone);
+        System.out.print("\n Оклад : " + salary);
+        System.out.print("\n Возраст : " + age);
+
+    }
+    void Show()
+    {
+        System.out.println(name + " " + position + " " + email + " " + phone + " " + salary + " " + age);
+    }
+
 }
